63a9e0415b557dedc7899812c173d2f7da119e39
AL03F13 <aponteeric95@gmail.com>
2015-06-25 09:53:25 -0500

    tres
    js

---
+++ js/acciones.js
@@ -0,0 +1,28 @@
+// JavaScript Document
+$(document).ready(function(e) {
+document.addEventListener("deviceready",function(){
+	
+	$('#btnprincipal').on('tap',function(){
+	navigator.vibrate(1000);
+	});
+	
+	$('#btntipos').on('tap',function(){
+	navigator.vibrate(1000);
+	});
+	
+	$('#btnfactores').on('tap',function(){
+	navigator.vibrate(1000);
+	});
+	
+	$('#btntest').on('tap',function(){
+	navigator.vibrate(1000);
+	});
+	
+	$('#btngaleria').on('tap',function(){
+	navigator.vibrate(1000);
+	});
+	
+	
+}); 
+});
+
